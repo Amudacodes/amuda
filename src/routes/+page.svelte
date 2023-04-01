fb1ed1bf1ec1c0b17278dc2c0e9a55faa8af66a9
Amudacodes <ritzyimmortal@gmail.com>
2023-04-01 23:26:02 +0100

    hhh

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -169,7 +169,7 @@
 
 
 
-      <section class=" py-16">
+      <!-- <section class=" py-16">
         <div class="all mx-auto">
           <h2 class="text-3xl font-bold text-white mb-8">Some of My Work</h2>
           <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
@@ -213,7 +213,7 @@
                 <a href="#" class="bg-green-500 hover:bg-green-600 text-white px-4 py-2 rounded mt-4 inline-block">View project</a>
               </div>
             </div>
-            </section> 
+            </section>  -->
       
       
       
